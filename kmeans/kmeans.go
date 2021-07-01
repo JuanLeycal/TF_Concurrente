ad934387f50273f4cb22fc070616348564b88d32
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-06-30 22:50:52 -0500

    Refactored encoding and decoding to return []Node.

--- kmeans/kmeans.go
+++ kmeans/kmeans.go
@@ -21,7 +21,6 @@ func toString(nList []Node) string {
 			} else {
 				aux = append(aux, s+"-")
 			}
-
 		}
 		sNode = append(sNode, aux)
 	}
@@ -32,116 +31,42 @@ func toString(nList []Node) string {
 
 	return sLong
 }
-func toInt(value int) string {
-	sNum := strconv.Itoa(value)
 
-	return sNum
+func toInt(value int) string {
+	return strconv.Itoa(value)
 }
 
-func decodeNode(array []string) (nNode [][]float64) {
-
+func decodeNode(array []string) (nNode []Node) {
 	var example [][]string
 	for i := 0; i < len(array); i++ {
 		s := strings.Split(array[i], "-")
 		example = append(example, s)
 	}
 	for i := 0; i < len(example)-1; i++ {
-		var aux []float64
-		if len(example[i]) != 0 {
-			for j := 0; j < len(example[i]); j++ {
-				f, _ := strconv.ParseFloat(example[i][j], 32)
-				aux = append(aux, f)
-
-			}
-		}
-		nNode = append(nNode, aux)
+		auxN := Node{}
+		a, _ := strconv.ParseFloat(example[i][0], 32)
+		b, _ := strconv.ParseFloat(example[i][1], 32)
+		c, _ := strconv.ParseFloat(example[i][2], 32)
+		d, _ := strconv.ParseFloat(example[i][3], 32)
+		e, _ := strconv.ParseFloat(example[i][4], 32)
+		auxN = append(auxN, a, b, c, d, e)
+		nNode = append(nNode, auxN)
 	}
-
 	return
-}
-
-func firstEcode(nList []Node, nClusters int, maxIter int) string {
-
-	sLong := toString(nList)
-
-	t := toInt(nClusters)
-	p := toInt(maxIter)
-
-	sLong = sLong + ";" + t + ";" + p
-
-	return sLong
 }
 func trainingEncode(nList []Node, nCentroids []Node, maxIter int) string {
 	sLong := toString(nList)
 	sCentroid := toString(nCentroids)
 	p := toInt(maxIter)
-
 	sLong = sLong + ";" + sCentroid + ";" + p
-
 	return sLong
 }
 
-func firstDecode(sLong string) (recon [][]float64, newNCluster int, newNIter int) {
-
+func trainingDecode(sLong string) (recon []Node, newNCluster []Node, newNIter int) {
 	deConvert := strings.Split(sLong, ";")
-
 	deNode := strings.Split(deConvert[0], ",")
-
-	fmt.Println(deNode[0])
-
-	recon = decodeNode(deNode)
-
-	newNCluster, _ = strconv.Atoi(deConvert[1])
-	newNIter, _ = strconv.Atoi(deConvert[2])
-
-	// var example [][]string
-	// for i := 0; i < len(deNode); i++ {
-	// 	s := strings.Split(deNode[i], "-")
-	// 	example = append(example, s)
-	// }
-
-	// for i := 0; i < len(example)-1; i++ {
-	// 	var aux []float64
-	// 	if len(example[i]) != 0 {
-	// 		for j := 0; j < len(example[i]); j++ {
-	// 			f, _ := strconv.ParseFloat(example[i][j], 32)
-	// 			aux = append(aux, f)
-
-	// 		}
-	// 	}
-	// 	recon = append(recon, aux)
-	// }
-
-	return
-}
-
-func trainingDecode(sLong string) (recon [][]float64, newNCluster [][]float64, newNIter int) {
-
-	deConvert := strings.Split(sLong, ";")
-
-	deNode := strings.Split(deConvert[0], ",")
-
 	deCluster := strings.Split(deConvert[1], ",")
-
-	fmt.Println(deNode[1])
-
-	// var example [][]string
-	// for i := 0; i < len(deNode); i++ {
-	// 	s := strings.Split(deNode[i], "-")
-	// 	example = append(example, s)
-	// }
-
-	// for i := 0; i < len(example)-1; i++ {
-	// 	var aux []float64
-	// 	if len(example[i]) != 0 {
-	// 		for j := 0; j < len(example[i]); j++ {
-	// 			f, _ := strconv.ParseFloat(example[i][j], 32)
-	// 			aux = append(aux, f)
-
-	// 		}
-	// 	}
-	// 	recon = append(recon, aux)
-	// }
+	//fmt.Println(deNode[1])
 	recon = decodeNode(deNode)
 	newNCluster = decodeNode(deCluster)
 	newNIter, _ = strconv.Atoi(deConvert[2])
@@ -173,42 +98,17 @@ func KMeansInit(nList []Node, nClusters int, maxIter int) (bool, []Node) {
 		copy(nCentroids[i], nList[randN.Intn(lnList)])
 	}
 
-	//Aquí iria la primera codificación s tring de los nodos
-	sLong := firstEcode(nList, nClusters, maxIter)
-	// la función KmeansTraining llamaría a sólo un String
-	a, b, c := firstDecode(sLong)
-
-	fmt.Println("Original array")
-	fmt.Println(nList)
-	fmt.Println("Original number of clusters")
-	fmt.Println(nClusters)
-	fmt.Println("Original number of iteration")
-	fmt.Println(maxIter)
-	fmt.Println("array coded")
-	fmt.Println(sLong)
-	fmt.Println("Coded/Decoded array")
-	fmt.Println(a)
-	fmt.Println("Coded/Decoded number of clusters")
-	fmt.Println(b)
-	fmt.Println("Coded/Decoded number of iteration")
-	fmt.Println(c)
-
-	//return KMeansTraining(nodeStrings)
+	//reemplazar v por encoding y llamada
 	return KMeansTraining(nList, nCentroids, maxIter)
 }
 
 func KMeansTraining(nList []Node, nCentroids []Node, maxIter int) (bool, []Node) {
 	//Aquí se clerarían las variables usadas sólo en la función.
-	//Continuación iria la decodificación y divisón del string para asignarlo a sus variables
-
-	sTraining := trainingEncode(nList, nCentroids, maxIter)
-	fmt.Println(sTraining)
-	_, b, _ := trainingDecode(sTraining)
-	fmt.Println("Centroides decodeados")
-	fmt.Println(b)
+	//Continuación iria la decodificación y divisón del string para asignarlo a sus variables (externo)
 
 	//Training arc
 	canMove := true
+	//modificar para que haga una sola pasada
 	for i := 0; i < maxIter && canMove; i++ {
 		canMove = false
 		cluster := make(map[int][]Node)
@@ -218,13 +118,14 @@ func KMeansTraining(nList []Node, nCentroids []Node, maxIter int) (bool, []Node)
 		}
 		for key, value := range cluster {
 			nNode := meanNode(value)
-			if equals(nCentroids[key], nNode) == false {
+			if !equals(nCentroids[key], nNode) {
 				nCentroids[key] = nNode
 				canMove = true
 			}
 		}
 	}
-	//codificar again en String los nodos
+	//codificar again en String los nodos (externo)
+	// retornar los mismos parametros, nList, nCentroids, maxIter
 	return true, nCentroids
-	//return false, KMeansTraining(nodeStrings)
+
 }
